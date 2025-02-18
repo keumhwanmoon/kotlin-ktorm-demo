b4e29f43f09cca3ad0de8ba52494d167ae644c43
keumhwan.moon <keumhwan.moon@lgepartner.com>
2025-02-19 07:42:15 +0900

    Add database schema, seed data, and HTTP request example

--- src/main/kotlin/com/demo/ktorm/user/service/UserService.kt
+++ src/main/kotlin/com/demo/ktorm/user/service/UserService.kt
@@ -7,35 +7,35 @@ import org.ktorm.dsl.from
 import org.ktorm.dsl.map
 import org.ktorm.dsl.select
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserService(
     private val database: Database
 ) {
+    @Transactional
     fun getAllUsers(): List<UserRes> {
-        return database.useTransaction {
-            database
-                .from(Users)
-                .select(
-                    Users.id,
-                    Users.loginId,
-                    Users.userName,
-                    Users.picture,
-                    Users.phoneNumber,
-                    Users.mobilePhoneNumber,
-                    Users.email
+        return database
+            .from(Users)
+            .select(
+                Users.id,
+                Users.loginId,
+                Users.userName,
+                Users.picture,
+                Users.phoneNumber,
+                Users.mobilePhoneNumber,
+                Users.email
+            )
+            .map { row ->
+                UserRes(
+                    id = row[Users.id],
+                    loginId = row[Users.loginId]!!,
+                    userName = row[Users.userName],
+                    picture = row[Users.picture],
+                    phoneNumber = row[Users.phoneNumber],
+                    mobilePhoneNumber = row[Users.mobilePhoneNumber],
+                    email = row[Users.email]
                 )
-                .map { row ->
-                    UserRes(
-                        id = row[Users.id],
-                        loginId = row[Users.loginId]!!,
-                        userName = row[Users.userName],
-                        picture = row[Users.picture],
-                        phoneNumber = row[Users.phoneNumber],
-                        mobilePhoneNumber = row[Users.mobilePhoneNumber],
-                        email = row[Users.email]
-                    )
-                }
-        }
+            }
     }
 }
