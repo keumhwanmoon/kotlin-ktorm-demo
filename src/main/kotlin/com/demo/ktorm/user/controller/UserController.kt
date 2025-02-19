31b233202a561ea0df568a084aceae916e3a9bb9
keumhwan.moon <keumhwan.moon@lgepartner.com>
2025-02-19 10:12:38 +0900

    사용자 조회 기능 및 HTTP 요청 템플릿 개선
    사용자 ID로 조회하는 기능과 관련 헬퍼 메서드를 추가. HTTP 요청 템플릿에 변수화를 적용하고, 단일 사용자 조회 예제를 포함.

--- src/main/kotlin/com/demo/ktorm/user/controller/UserController.kt
+++ src/main/kotlin/com/demo/ktorm/user/controller/UserController.kt
@@ -6,16 +6,41 @@ import com.demo.ktorm.user.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
+/**
+ * Controller class to handle REST API endpoints for users.
+ */
 @RestController
 class UserController(
     private val userService: UserService
 ) {
 
+    /**
+     * Retrieves all users.
+     *
+     * @return a list of user response objects.
+     */
     @GetMapping("/api/users")
     fun getUsers(): List<UserRes> {
         return userService.getAllUsers()
     }
 
+    /**
+     * Retrieves a user by their ID.
+     *
+     * @param id the ID of the user to retrieve.
+     * @return the user response object.
+     */
+    @GetMapping("/api/users/{id}")
+    fun getUser(@PathVariable id: Long): UserRes {
+        return userService.getUser(id)
+    }
+
+    /**
+     * Registers a new user.
+     *
+     * @param request the user request object containing user details.
+     * @return the created user response object.
+     */
     @PostMapping("/api/users")
     @ResponseStatus(HttpStatus.CREATED)
     fun registerUser(@RequestBody request: UserReq): UserRes {
--- src/main/kotlin/com/demo/ktorm/user/service/UserService.kt
+++ src/main/kotlin/com/demo/ktorm/user/service/UserService.kt
@@ -13,6 +13,11 @@ import java.time.LocalDateTime
 class UserService(
     private val database: Database
 ) {
+    /**
+     * Retrieves all users from the database.
+     *
+     * @return a list of user response objects.
+     */
     @Transactional
     fun getAllUsers(): List<UserRes> {
         return database
@@ -39,6 +44,22 @@ class UserService(
             }
     }
 
+    /**
+     * Retrieves a single user by their ID.
+     *
+     * @param id the ID of the user to retrieve.
+     * @return the user response object.
+     */
+    fun getUser(id: Long): UserRes {
+        return userById(id)
+    }
+
+    /**
+     * Creates a new user in the database.
+     *
+     * @param request the user request object containing user details.
+     * @return the created user response object.
+     */
     @Transactional
     fun createUser(request: UserReq): UserRes {
         val now = LocalDateTime.now()
@@ -54,26 +75,34 @@ class UserService(
             set(Users.lastUpdatedAt, now)
         } as Long
 
-        return database.from(Users)
-            .select(
-                Users.id,
-                Users.loginId,
-                Users.userName,
-                Users.picture,
-                Users.phoneNumber,
-                Users.mobilePhoneNumber,
-                Users.email
-            ).where { Users.id eq id }
-            .map { row ->
-                UserRes(
-                    id = row[Users.id],
-                    loginId = row[Users.loginId]!!,
-                    userName = row[Users.userName],
-                    picture = row[Users.picture],
-                    phoneNumber = row[Users.phoneNumber],
-                    mobilePhoneNumber = row[Users.mobilePhoneNumber],
-                    email = row[Users.email]
-                )
-            }.first()
+        return userById(id)
     }
+
+    /**
+     * Helper method to fetch a user by their ID.
+     *
+     * @param id the ID of the user to retrieve.
+     * @return the user response object.
+     */
+    private fun userById(id: Long) = database.from(Users)
+        .select(
+            Users.id,
+            Users.loginId,
+            Users.userName,
+            Users.picture,
+            Users.phoneNumber,
+            Users.mobilePhoneNumber,
+            Users.email
+        ).where { Users.id eq id }
+        .map { row ->
+            UserRes(
+                id = row[Users.id],
+                loginId = row[Users.loginId]!!,
+                userName = row[Users.userName],
+                picture = row[Users.picture],
+                phoneNumber = row[Users.phoneNumber],
+                mobilePhoneNumber = row[Users.mobilePhoneNumber],
+                email = row[Users.email]
+            )
+        }.first()
 }
